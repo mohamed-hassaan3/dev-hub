69f2d9b6285b5c064c11b4a242512561258388bf
Mohamed Hassaan <mmhassaan3@gmail.com>
2025-02-22 13:12:08 +0400

    added loading spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const loading = () => {
   return (
-    <div className="min-h-screen p-3 animate-spin drop-shadow-2xl bg-gradient-to-bl from-pink-400 via-purple-400 to-indigo-600 md:w-32 md:h-32 h-16 w-16 aspect-square rounded-full">
+    <div className="md:p-2 p-1 animate-spin drop-shadow-2xl bg-gradient-to-bl from-pink-400 via-purple-400 to-indigo-600 md:w-16 md:h-16 h-12 w-12 aspect-square rounded-full absolute left-1/2 top-1/2">
       <div className="rounded-full h-full w-full bg-slate-100 dark:bg-zinc-900 background-blur-md"></div>
     </div>
   );
